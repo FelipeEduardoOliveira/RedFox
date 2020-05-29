a06ed151ea6d770f84aa49313d4aaaa1fe1a9c9a
Felipe Eduardo Rodrigues de Oliveira <59943007+FelipeEduardoOliveira@users.noreply.github.com>
2020-05-29 08:53:56 -0300

    Add files via upload

---
+++ build/precache-manifest.b0fcf5ac840907966774f5ee94f3704a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c7d6b009bcd383bdce5ce652c33874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e6826d64272dff6a2d",
+    "url": "/static/css/main.d4690133.chunk.css"
+  },
+  {
+    "revision": "9be409233abd43281525",
+    "url": "/static/js/2.c4c2b66d.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.c4c2b66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e6826d64272dff6a2d",
+    "url": "/static/js/main.8ab1423a.chunk.js"
+  },
+  {
+    "revision": "ec2f766c12cd5b84bab3",
+    "url": "/static/js/runtime-main.990a1ebd.js"
+  }
+]);
